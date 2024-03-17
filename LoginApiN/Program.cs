4d58d181b922bd64301cfd0412d7109a600a0a4d
UditShivhare <udit.shivhare@gmail.com>
2024-03-17 12:40:32 +0530

    LoginApiN commit first pc

--- LoginApiN/Program.cs
+++ LoginApiN/Program.cs
@@ -4,6 +4,8 @@ using LoginApiN.Data;
 using LoginApiN.Repository;
 using Microsoft.EntityFrameworkCore;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,7 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<LoginAPINContext>(options => options.UseSqlServer());
+//var connection = Configuration["DbConnectionString"];
+//builder.Services.AddDbContext<LoginAPINContext>(options => options.UseSqlServer());
 builder.Services.AddDbContext<LoginAPINContext>(options => options.UseSqlServer("Server=DESKTOP-LK9QR3K\\SQLEXPRESS01;Database=LoginAPIN;Integrated Security=true"));
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 
